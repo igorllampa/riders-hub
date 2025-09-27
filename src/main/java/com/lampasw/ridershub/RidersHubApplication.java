049653d51c36badc5e53e6da873657229b914f38
igorl <igorllampa@gmail.com>
2025-09-27 18:49:44 -0300

    chore: initialize spring boot project with spring initializr

---
+++ src/main/java/com/lampasw/ridershub/RidersHubApplication.java
@@ -0,0 +1,13 @@
+package com.lampasw.ridershub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidersHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidersHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lampasw/ridershub/RidersHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lampasw.ridershub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidersHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
